149f2d01ffbc52765c41620ccc84e6a5d9d205f9
Thomas Young <thomas.young228@gmail.com>
2017-08-29 00:32:54 -0400

    finished basic front end of full website

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ $(function() {
     } else{
       $("body").removeClass("sticky-shrinknav-wrapper");
     }
+    
   });
 });
 
+
+$('.form-floating-label input, .form-floating-label textarea').focusin(function(){
+  $(this).parent().addClass('has-value');
+});
+
+$('.form-floating-label input, .form-floating-label textarea').blur(function(){
+  if(!$(this).val().length > 0) {
+    $(this).parent().removeClass('has-value');
+  }
+});
 
